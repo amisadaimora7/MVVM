8b562caf3a90f00f2b8b58fab01f77c5f342e5ce
AmisadaiMora <amisadaimora7@gmail.com>
2017-11-29 16:26:24 -0600

    act

---
+++ Estados/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Estados
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {   
+        private object DataGridEstados;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Estados_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+
+        private void BUSCAR_Click(object sender, RoutedEventArgs e)
+        {
+            string CadenaDeConexion = @"Data Source=DESKTOP-BDSL3Q9;Initial Catalog=Municipios;Integrated Security=True";
+            string sql = "select * from Estado";
+            DataTable tabla = new DataTable();
+            SqlConnection conexion = new SqlConnection(CadenaDeConexion);
+            SqlDataAdapter datos = new SqlDataAdapter();
+            datos.SelectCommand = new SqlCommand(sql, conexion);
+            datos.Fill(tabla);
+            Estados.DataContext = tabla;
+            conexion.Close();
+        }
+    }
+}
---
+++ Estados/ViewModel/EstadosViewModel.cs
@@ -0,0 +1,63 @@
+﻿using Estados.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estados.ViewModel
+{
+    class EstadosViewModel : ViewModelBase
+    {
+        ObservableCollection<Estado> estados;
+        ObservableCollection<Municipio> municipios;
+        ObservableCollection<Localidad> localidades;
+        Estado estadoSeleccionado;
+        Municipio municipioSeleccionado;
+
+        public EstadosViewModel() {
+            this.estados = new ObservableCollection<Estado>();
+            MunicipiosEntities DB = new MunicipiosEntities();
+            var estaditos = DB.Estado.All();
+        }
+
+        public ObservableCollection<Estado> Estados
+        {
+            get { return estados; }
+            set
+            {
+                estados = value;
+                OnPropertyChanged("Estados");
+            }
+        }
+        public ObservableCollection<Municipio> Municipios
+        {
+            get { return municipios; }
+            set
+            {
+                municipios = value;
+                OnPropertyChanged("Municipios");
+            }
+        }
+        public ObservableCollection<Localidad> Localidades
+        {
+            get { return localidades; }
+            set
+            {
+                localidades = value;
+                OnPropertyChanged("Localidades");
+            }
+        }
+
+        public Estado EstadosSeleccionado {
+            get { return estadoSeleccionado; }
+            set { estadoSeleccionado = value;
+                Municipios = new ObservableCollection<Municipio>(estadoSeleccionado.Municipio);
+                OnPropertyChanged("EstadoSeleccionado");
+            }
+        }
+
+
+     }
+ }
